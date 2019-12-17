81187fcb5f6c9c1a44f70b4728418d0b9cef427c
Colin Burreson <csburreson@icecube.wisc.edu>
2019-12-17 15:31:31 -0600

    fixes #6

--- stf/testclasses.py
+++ stf/testclasses.py
@@ -3,6 +3,7 @@ import openhtf as htf
 from openhtf.output.callbacks.json_factory import OutputToJSON as JSON
 from openhtf.output.callbacks import console_summary
 from .core import getIcebootSession
+from .validators import *
 import stf
 FAKE_ICEBOOT = False
 
@@ -10,36 +11,8 @@ class TestSet(object):
     def __init(self, version):
         pass
 
-@htf.util.validators.register
-def equalsParam(pname, type=None):
-    if not (pname.startswith('{') and pname.endswith('}')):
-        pname = '{' + pname + '}'
-    return EqualsParam(pname, type=type)
 
 
-class EqualsParam(htf.util.validators.ValidatorBase):
-    def __init__(self, pvalue, type=None):
-        self.paramValue = pvalue
-        self._type = type
-
-    def __call__(self, value):
-        return self.paramValue == value
-        
-    def __str__(self):
-        '''use in output'''
-        return 'x == {}'.format(self.paramValue)
-
-
-    def with_args(self, **kw):
-        try:
-            stf.debug('kw keys: {}'.format(kw.keys()))
-            stf.debug('type: {}'.format(dir(self)))
-            return type(self)(
-                pvalue=htf.util.format_string(self.paramValue, self.expectedValues),
-                type=kw.get('type', None),
-            )
-        except KeyError:
-            raise Exception('Framework Error. Contact maintainer')
 
 class Common(object):
     # default test config options
@@ -198,6 +171,7 @@ class MainboardTest(object):
 
         for m in self.test_fn.measurements:
             for v in m.validators:
+                stf.debug('added ev to {}'.format(type(v)))
                 setattr(v, 'expectedValues', expected_values)
 
         phases = [
