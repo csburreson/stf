f725b92f3fa733ebe826a30b294e2429b1cf8243
Colin Burreson <csburreson@icecube.wisc.edu>
2019-12-21 12:41:09 -0800

    added setconfig stuff and dev "g" files

---
+++ tests/g2.py
@@ -0,0 +1,10 @@
+import stf
+import re as barf
+
+def run_test(test, session, foo=None, bar=None, czar="King"):
+    barf.compile
+
+stf.register(run=run_test, test_name='g2', version='1.0')
+
+if __name__ == '__main__':
+    stf.run()
