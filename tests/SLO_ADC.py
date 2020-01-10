acbb38d1cef43178d3b5c349ec5357ac82b38a8b
Jeff Weber <jeff.weber@icecube.wisc.edu>
2020-01-10 10:05:31 -0600

    SLO_ADC integration testing.

--- tests/SLO_ADC.py
+++ tests/SLO_ADC.py
@@ -50,16 +50,21 @@ def readSloAdcSingle(test, session, channel):
 
 # Test SLO_ADC single channel reads.
 @stf.measures(
-    stf.Measurement('chan_00').expect(
-        "{channel 00 LVS_SLO_IMON_1V1}"), 
-    stf.Measurement('chan_01').expect(
-        "{channel 01 LVS_SLO_IMON_1V35}"),
-    stf.Measurement('chan_02').expect(
-        "{channel 02 LVS_SLO_IMON_1V8}"),
-    stf.Measurement('chan_03').expect(
-        "{channel 03 LVS_SLO_IMON_2V5}"),
-    stf.Measurement('chan_04').expect(
-        "{channel 04 LVS_SLO_IMON_3V3}"),
+    stf.Measurement('chan_00').expectPercent(
+        "{channel 00 LVS_SLO_IMON_1V1}",
+        "{current_percent}"),
+    stf.Measurement('chan_01').expectPercent(
+        "{channel 01 LVS_SLO_IMON_1V35}",
+        "{current_percent}"),
+    stf.Measurement('chan_02').expectPercent(
+        "{channel 02 LVS_SLO_IMON_1V8}",
+        "{current_percent}"),
+    stf.Measurement('chan_03').expectPercent(
+        "{channel 03 LVS_SLO_IMON_2V5}",
+        "{current_percent}"),
+    stf.Measurement('chan_04').expectPercent(
+        "{channel 04 LVS_SLO_IMON_3V3}",
+        "{current3_3_percent}"),
     stf.Measurement('chan_05').expectPercent(
         "{channel 05 +1V8_A}",
         "{voltage_rail_percent}"),
@@ -71,16 +76,16 @@ def readSloAdcSingle(test, session, channel):
         "{channel 07 temperature_room_min}",
         "{channel 07 temperature_room_max}",
         type=float),
-    stf.Measurement('chan_08').expectPercent(
-        "{channel 08 SLO_HVS0_VMON}",
-        "{voltage_rail_percent}"),
-    stf.Measurement('chan_09').expect(
-        "{channel 09 SLO_HVS0_IMON}"),
-    stf.Measurement('chan_10').expectPercent(
-        "{channel 10 SLO_HVS1_VMON}",
-        "{voltage_rail_percent}"),
-    stf.Measurement('chan_11').expect(
-        "{channel 11 SLO_HVS1_IMON}"),
+    #stf.Measurement('chan_08').expectPercent(
+    #    "{channel 08 SLO_HVS0_VMON}",
+    #    "{voltage_rail_percent}"),
+    #stf.Measurement('chan_09').expect(
+    #    "{channel 09 SLO_HVS0_IMON}"),
+    #stf.Measurement('chan_10').expectPercent(
+    #    "{channel 10 SLO_HVS1_VMON}",
+    #    "{voltage_rail_percent}"),
+    #stf.Measurement('chan_11').expect(
+    #    "{channel 11 SLO_HVS1_IMON}"),
     stf.Measurement('chan_12').expectPercent(
         "{channel 12 +1V1_power_rail_monitor}",
         "{voltage_rail_percent}"),
@@ -105,10 +110,10 @@ def test_single_channel(test, session, **kw):
     test.measurements.chan_05 = readSloAdcSingle(test, session,  5)
     test.measurements.chan_06 = readSloAdcSingle(test, session,  6)
     test.measurements.chan_07 = readSloAdcSingle(test, session,  7)
-    test.measurements.chan_08 = readSloAdcSingle(test, session,  8)
-    test.measurements.chan_09 = readSloAdcSingle(test, session,  9)
-    test.measurements.chan_10 = readSloAdcSingle(test, session, 10)
-    test.measurements.chan_11 = readSloAdcSingle(test, session, 11)
+    #test.measurements.chan_08 = readSloAdcSingle(test, session,  8)
+    #test.measurements.chan_09 = readSloAdcSingle(test, session,  9)
+    #test.measurements.chan_10 = readSloAdcSingle(test, session, 10)
+    #test.measurements.chan_11 = readSloAdcSingle(test, session, 11)
     test.measurements.chan_12 = readSloAdcSingle(test, session, 12)
     test.measurements.chan_13 = readSloAdcSingle(test, session, 13)
     test.measurements.chan_14 = readSloAdcSingle(test, session, 14)
@@ -118,16 +123,21 @@ def test_single_channel(test, session, **kw):
 
 # Test SLO_ADC read all channels
 @stf.measures(
-    stf.Measurement('chan_00').expect(
-        "{channel 00 LVS_SLO_IMON_1V1}"), 
-    stf.Measurement('chan_01').expect(
-        "{channel 01 LVS_SLO_IMON_1V35}"),
-    stf.Measurement('chan_02').expect(
-        "{channel 02 LVS_SLO_IMON_1V8}"),
-    stf.Measurement('chan_03').expect(
-        "{channel 03 LVS_SLO_IMON_2V5}"),
-    stf.Measurement('chan_04').expect(
-        "{channel 04 LVS_SLO_IMON_3V3}"),
+    stf.Measurement('chan_00').expectPercent(
+        "{channel 00 LVS_SLO_IMON_1V1}", 
+        "{current_percent}"),
+    stf.Measurement('chan_01').expectPercent(
+        "{channel 01 LVS_SLO_IMON_1V35}",
+        "{current_percent}"),
+    stf.Measurement('chan_02').expectPercent(
+        "{channel 02 LVS_SLO_IMON_1V8}",
+        "{current_percent}"),
+    stf.Measurement('chan_03').expectPercent(
+        "{channel 03 LVS_SLO_IMON_2V5}",
+        "{current_percent}"),
+    stf.Measurement('chan_04').expectPercent(
+        "{channel 04 LVS_SLO_IMON_3V3}",
+        "{current3_3_percent}"),
     stf.Measurement('chan_05').expectPercent(
         "{channel 05 +1V8_A}",
         "{voltage_rail_percent}"),
@@ -139,16 +149,16 @@ def test_single_channel(test, session, **kw):
         "{channel 07 temperature_room_min}",
         "{channel 07 temperature_room_max}",
         type=float),
-    stf.Measurement('chan_08').expectPercent(
-        "{channel 08 SLO_HVS0_VMON}",
-        "{voltage_rail_percent}"),
-    stf.Measurement('chan_09').expect(
-        "{channel 09 SLO_HVS0_IMON}"),
-    stf.Measurement('chan_10').expectPercent(
-        "{channel 10 SLO_HVS1_VMON}",
-        "{voltage_rail_percent}"),
-    stf.Measurement('chan_11').expect(
-        "{channel 11 SLO_HVS1_IMON}"),
+#    stf.Measurement('chan_08').expectPercent(
+#        "{channel 08 SLO_HVS0_VMON}",
+#        "{voltage_rail_percent}"),
+#    stf.Measurement('chan_09').expect(
+#        "{channel 09 SLO_HVS0_IMON}"),
+#    stf.Measurement('chan_10').expectPercent(
+#        "{channel 10 SLO_HVS1_VMON}",
+#        "{voltage_rail_percent}"),
+#    stf.Measurement('chan_11').expect(
+#        "{channel 11 SLO_HVS1_IMON}"),
     stf.Measurement('chan_12').expectPercent(
         "{channel 12 +1V1_power_rail_monitor}",
         "{voltage_rail_percent}"),
@@ -184,13 +194,13 @@ stf.register(
     config_file = "data/testconfig/SLO_ADC.json",
 )
 
-stf.register(
-    version = '1.0',
-    run = test_all_channel,
-    test_name = "SLO_ADC_ALL_CHANNEL",
-    test_desc = "SLO ADC all channel reads",
-    config_file = "data/testconfig/SLO_ADC.json",
-)
+#stf.register(
+#    version = '1.0',
+#    run = test_all_channel,
+#    test_name = "SLO_ADC_ALL_CHANNEL",
+#    test_desc = "SLO ADC all channel reads",
+#    config_file = "data/testconfig/SLO_ADC.json",
+#)
 
 
 if __name__ == '__main__':
