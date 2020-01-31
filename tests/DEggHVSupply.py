abd45ad01326fba77981e08cff1cba03e37b6e92
Ken'ichi KIN <31651539+KIN-KEN-ICHI@users.noreply.github.com>
2020-01-31 18:35:25 +0900

    Add DEggHVSupply.py

---
+++ tests/DEggHVSupply.py
@@ -0,0 +1,44 @@
+import stf
+import time
+
+@stf.measures(stf.M('DEggHVSupply0').expectRange('{expected_degghvsupply_min}','{expected_degghvsupply_max}',type=float),
+              stf.M('DEggCurrent0').expectRange('{expected_deggcurrent_min}','{expected_deggcurrent_max}',type=float),
+              stf.M('DEggHVSupply1').expectRange('{expected_degghvsupply_min}','{expected_degghvsupply_max}',type=float),
+              stf.M('DEggCurrent1').expectRange('{expected_deggcurrent_min}','{expected_deggcurrent_max}',type=float))
+
+def run_test(test, session):
+    '''this test checks the applied HV voltage is correct'''
+    #CH0
+    readVal(test,session,0)
+    #CH1
+    readVal(test,session,1)
+
+stf.register(
+    version='1.0',
+    test_name='DEggHVSupply',
+    run=run_test,
+)
+
+def readVal(test, session, ch):
+    session.setDEggHV(ch, 200)
+    session.enableHV(ch)
+    time.sleep(0.2)
+    observed_hvvalue = session.sloAdcReadChannel(8+ch*2)
+    time.sleep(0.2)
+    observed_current = session.sloAdcReadChannel(9+ch*2)
+    if ch == 0:
+        test.logger.info('Measured HV value for ch0: {}'.format(observed_hvvalue))
+        test.measurements.DEggHVSupply0 = observed_hvvalue
+        test.logger.info('Measured Current value for ch0: {}'.format(observed_current))
+        test.measurements.DEggCurrent0 = observed_current
+    else:
+        test.logger.info('Measured HV value for ch0: {}'.format(observed_hvvalue))
+        test.measurements.DEggHVSupply1 = observed_hvvalue
+        test.logger.info('Measured Current value for ch0: {}'.format(observed_current))
+        test.measurements.DEggCurrent1 = observed_current
+    session.disableHV(ch)
+
+
+# use "python <this-file.py>" to run
+if __name__ == '__main__':
+    stf.run()
