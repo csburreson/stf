6ebd863cdbe381142565c41ea9315834d54b4f84
Tianlu Yuan <tianlutheman@gmail.com>
2020-02-11 14:16:02 -0600

    Fix issue when looking for rbf file in checkCommsAndFirmware

--- stf/testclasses.py
+++ stf/testclasses.py
@@ -51,7 +51,7 @@ class Common(object):
             gINFO(f'checkCommsAndFirmware -> SKIPPING fw file upload, configuring: {paths.fwfile_name}')
         else:
             gINFO(f'checkCommsAndFirmware -> uploading fw file to flash: {paths.fwfile}... \n\t(this could take a while)')
-            session.ymodemFlashUpload(paths.fwfile, paths.fwfile_name)
+            session.ymodemFlashUpload(paths.fwfile_name, paths.fwfile)
 
         session.flashConfigureCycloneFPGA(paths.fwfile_name)
 
